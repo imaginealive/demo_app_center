d5d1533ad5fa7e125496da82d0915fa8c2fb3e71
Anawat <Anawat@perfenterprise.com>
2017-12-14 09:50:29 +0700

    add project to git
    Signed-off-by: Anawat <Anawat@perfenterprise.com>

---
+++ demo_appcenter/demo_appcenter/demo_appcenter/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace demo_appcenter
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
